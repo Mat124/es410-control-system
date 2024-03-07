09df65d70de9602ae8203f061940e0c95213232c
Mikolaj Dworzycki <56508855+MikDwor@users.noreply.github.com>
2024-03-07 00:01:46 +0000

    Add files via upload

--- qr-scanning/camera_calibration.py
+++ qr-scanning/camera_calibration.py
@@ -1,3 +1,4 @@
+# Code from: https://docs.opencv.org/4.x/dc/dbb/tutorial_py_calibration.html
 import numpy as np
 import cv2 as cv
 import glob
@@ -12,11 +13,12 @@ objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
-images = glob.glob('*.jpg')
+images = glob.glob('*.jpg') # collect all images in the current folder that arre .jpg type
 
 for fname in images:
+
     img = cv.imread(fname)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY) # convert the colour image into greyscale
     
     # Find the chess board corners
     ret, corners = cv.findChessboardCorners(gray, (7,6), None)
@@ -29,22 +31,21 @@ for fname in images:
 
         # Draw and display the corners
         cv.drawChessboardCorners(img, (7,6), corners2, ret)
-        #cv.imshow('img', img)
 
-        # Save the new pattern
+        # Save the new pattern onto the original image
         cv.imwrite(f'pattern_{fname}',img)
         cv.waitKey(1)
 cv.destroyAllWindows()
 
+# Calibrate the camera to obtain the important camera intrinsic property matrix and distortion parameters
 ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
-print(mtx, '\n')
-print(dist)
-
+# Write the camera matrix and distortion parameters to a .dat file on the same directory as this program:
 filename = 'cam_properties.dat'
 
-f = open(filename, 'w')
+f = open(filename, 'w') # Open file
 
+# Line by line add the data
 for i in range(6):
     if i == 0 :
         f.write('Intrinsic: \n')
@@ -55,4 +56,4 @@ for i in range(6):
     else:
         f.write(str(dist))
 
-f.close()
+f.close() # Close file
